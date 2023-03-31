901d0e8e1ef562fc8bfec6e08a3c22a4e8da7522
Jefferson Neves <jefferson.rpn@gmail.com>
2023-03-30 23:09:00 -0300

    site url

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,6 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://novosurbanoscg.github.io/',
+  site: 'https://novosurbanoscg.github.io/site',
   integrations: [mdx(), sitemap(), tailwind()]
 });
